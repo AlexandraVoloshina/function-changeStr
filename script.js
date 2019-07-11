6faadb2feff9ad3d39d9ff002f0048eeb8d4e6e8
Alexandra <alexandrita@ukr.net>
2019-07-11 17:24:13 +0300

    Add

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function changeStr(str_) {
-	var str = str_.toLowerCase();
-	var str2 = str.split(" ");
-	var str3 = [];
-	for (var i = 0; i < str2.length; i++){
-		if (!str2[i]) return str2[i];
-  		str3[i] = str2[i][0].toUpperCase() + str2[i].slice(1);
-	}
-	console.log(str3.join(" "));
+    var str = str_.toLowerCase();
+    var str2 = str.split(" ");
+    var str3 = [];
+    for (var i = 0; i < str2.length; i++){
+        if (!str2[i]) return str2[i];
+          str3[i] = str2[i][0].toUpperCase() + str2[i].slice(1);
+    }
+    alert(str3.join(" "));
 }
 
-changeStr("гайда денис сергеевич");
-changeStr("ГАЙДА ДЕНИС СЕРГЕЕВИЧ");
-changeStr("гайда ДЕНИС сергеевич");
+var str_User = prompt("Ваше ФИО полностью?");
+changeStr(str_User);
+
+//changeStr("гайда денис сергеевич");
+//changeStr("ГАЙДА ДЕНИС СЕРГЕЕВИЧ");
+//changeStr("гайда ДЕНИС сергеевич");
 
 
